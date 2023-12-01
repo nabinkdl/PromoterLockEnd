bca33e1d17892bf82f234fe14d77398312dfd801
Sanchit Poudel <snchtpdl12@gmail.com>
2023-12-01 22:35:44 +0545

    update 5 files

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 
-function Row(props) {
+function Row({SN,SYMBOL,AllotmentDate,NumberOfShares,PromoterShares,PromoterHolding,LockendDate}) {
 
- console.log(props)
 
   return (
 
 <>
 
       <tr>
-          <th>{props.SN}</th> 
-          <td>{props.SYMBOL}</td> 
-          <td>{props.AllotmentDate}</td> 
-          <td>{props.NumberOfShares}</td> 
-          <td>{props.PromoterShares}</td> 
-          <td>{props.PromoterHolding}</td> 
-          <td>{props.LockendDate}</td>
+          <th>{SN}</th> 
+          <td>{SYMBOL}</td> 
+          <td>{AllotmentDate}</td> 
+          <td>{NumberOfShares}</td> 
+          <td>{PromoterShares}</td> 
+          <td>{PromoterHolding}</td> 
+          <td>{LockendDate}</td> 
           {/* <td>04</td> */}
         </tr>
 
