f9b685e6ecfeef725b72579b18db7400d8c699e0
“Nabin <nabinkandel@student.pu.edu.np>
2023-12-01 21:27:38 +0545

    feat: :sparkles:

---
+++ src/Row.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Row() {
+  return (
+    <div>
+
+        <tr>
+          <th>1</th> 
+          <td>Cy Ganderton</td> 
+          <td>Quality Control Specialist</td> 
+          <td>Littel, Schaden and Vandervort</td> 
+          <td>Canada</td> 
+          <td>12/16/2020</td> 
+          <td>Blue</td>
+        </tr>
+    </div>
+  )
+}
+
+export default Row
