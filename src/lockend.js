0adf4779aae3e936ea4250c9d1785802936c91da
“Nabin <nabinkandel@student.pu.edu.np>
2023-12-02 19:46:59 +0545

    update lockend.js

--- src/lockend.js
+++ src/lockend.js
@@ -25,7 +25,7 @@ export default
           "NumberOfShares":15340910,
           "PromoterShares":13500000,
           "PromoterHolding":88,
-          "LockendDate":"2024-01-24"
+          "LockendDate":"2024-01-22"
        },
        {
           "SN":4,
@@ -34,7 +34,7 @@ export default
           "NumberOfShares":14500000,
           "PromoterShares":10150000,
           "PromoterHolding":70,
-          "LockendDate":"2024-11-10"
+          "LockendDate":"2024-04-09"
        },
        {
           "SN":5,
@@ -43,7 +43,7 @@ export default
           "NumberOfShares":4933236,
           "PromoterShares":3946589,
           "PromoterHolding":80,
-          "LockendDate":"2024-11-17"
+          "LockendDate":"2024-04-16"
        },
        {
           "SN":6,
@@ -52,7 +52,7 @@ export default
           "NumberOfShares":8394100,
           "PromoterShares":5875870,
           "PromoterHolding":70,
-          "LockendDate":"2024-12-09"
+          "LockendDate":"2024-05-14"
        },
        {
           "SN":7,
@@ -61,7 +61,7 @@ export default
           "NumberOfShares":3681430,
           "PromoterShares":2577001,
           "PromoterHolding":70,
-          "LockendDate":"2024-08-11"
+          "LockendDate":"2024-08-10"
        },
        {
           "SN":8,
@@ -70,7 +70,7 @@ export default
           "NumberOfShares":4000000,
           "PromoterShares":2800000,
           "PromoterHolding":70,
-          "LockendDate":"2024-09-12"
+          "LockendDate":"2024-09-10"
        },
        {
           "SN":9,
@@ -79,7 +79,7 @@ export default
           "NumberOfShares":35000000,
           "PromoterShares":24500000,
           "PromoterHolding":70,
-          "LockendDate":"2024-11-06"
+          "LockendDate":"2024-10-03"
        },
        {
           "SN":10,
@@ -88,7 +88,7 @@ export default
           "NumberOfShares":4000000,
           "PromoterShares":2800000,
           "PromoterHolding":70,
-          "LockendDate":"2024-11-11"
+          "LockendDate":"2024-10-08"
        },
        {
           "SN":11,
@@ -97,7 +97,7 @@ export default
           "NumberOfShares":15000000,
           "PromoterShares":10950000,
           "PromoterHolding":73,
-          "LockendDate":"2024-11-25"
+          "LockendDate":"2024-10-22"
        },
        {
           "SN":12,
@@ -106,7 +106,7 @@ export default
           "NumberOfShares":60000000,
           "PromoterShares":30600000,
           "PromoterHolding":51,
-          "LockendDate":"2024-12-02"
+          "LockendDate":"2024-10-29"
        },
        {
           "SN":13,
@@ -115,7 +115,7 @@ export default
           "NumberOfShares":5000000,
           "PromoterShares":3770000,
           "PromoterHolding":75,
-          "LockendDate":"2024-12-06"
+          "LockendDate":"2021-11-02"
        },
        {
           "SN":14,
@@ -124,7 +124,7 @@ export default
           "NumberOfShares":31250000,
           "PromoterShares":25000000,
           "PromoterHolding":80,
-          "LockendDate":"2025-03-31"
+          "LockendDate":"2025-04-03"
        },
        {
           "SN":15,
@@ -133,7 +133,7 @@ export default
           "NumberOfShares":18279700,
           "PromoterShares":14623760,
           "PromoterHolding":80,
-          "LockendDate":"2025-04-09"
+          "LockendDate":"2025-04-12"
        },
        {
           "SN":16,
@@ -142,7 +142,7 @@ export default
           "NumberOfShares":7000000,
           "PromoterShares":3570000,
           "PromoterHolding":51,
-          "LockendDate":"2025-06-11"
+          "LockendDate":"2025-05-23"
        },
        {
           "SN":17,
@@ -151,7 +151,7 @@ export default
           "NumberOfShares":10500000,
           "PromoterShares":7875000,
           "PromoterHolding":75,
-          "LockendDate":"2025-07-06"
+          "LockendDate":"2025-06-17"
        },
        {
           "SN":18,
@@ -160,7 +160,7 @@ export default
           "NumberOfShares":10542604,
           "PromoterShares":7590674,
           "PromoterHolding":72,
-          "LockendDate":"2025-07-11"
+          "LockendDate":"2025-06-22"
        },
        {
           "SN":19,
@@ -169,7 +169,7 @@ export default
           "NumberOfShares":10950000,
           "PromoterShares":8699775,
           "PromoterHolding":79,
-          "LockendDate":"2025-07-31"
+          "LockendDate":"2025-07-12"
        },
        {
           "SN":20,
@@ -178,7 +178,7 @@ export default
           "NumberOfShares":13500000,
           "PromoterShares":10192500,
           "PromoterHolding":76,
-          "LockendDate":"2025-08-02"
+          "LockendDate":"2025-07-14"
        },
        {
           "SN":21,
@@ -187,7 +187,7 @@ export default
           "NumberOfShares":5000000,
           "PromoterShares":3000000,
           "PromoterHolding":60,
-          "LockendDate":"2025-08-04"
+          "LockendDate":"2025-07-16"
        },
        {
           "SN":22,
@@ -196,7 +196,7 @@ export default
           "NumberOfShares":6127938,
           "PromoterShares":3186527,
           "PromoterHolding":52,
-          "LockendDate":"2025-08-13"
+          "LockendDate":"2025-07-25"
        },
        {
           "SN":23,
@@ -205,7 +205,7 @@ export default
           "NumberOfShares":5880369,
           "PromoterShares":4116258,
           "PromoterHolding":70,
-          "LockendDate":"2025-08-20"
+          "LockendDate":"2025-08-02"
        },
        {
           "SN":24,
@@ -214,7 +214,7 @@ export default
           "NumberOfShares":15930000,
           "PromoterShares":13540500,
           "PromoterHolding":85,
-          "LockendDate":"2025-08-21"
+          "LockendDate":"2025-08-03"
        },
        {
           "SN":25,
@@ -223,7 +223,7 @@ export default
           "NumberOfShares":3000000,
           "PromoterShares":764700,
           "PromoterHolding":25,
-          "LockendDate":"2025-08-27"
+          "LockendDate":"2025-08-09"
        },
        {
           "SN":26,
@@ -232,7 +232,7 @@ export default
           "NumberOfShares":6326000,
           "PromoterShares":3226260,
           "PromoterHolding":51,
-          "LockendDate":"2025-09-15"
+          "LockendDate":"2025-09-28"
        },
        {
           "SN":27,
@@ -241,7 +241,7 @@ export default
           "NumberOfShares":8500000,
           "PromoterShares":6499950,
           "PromoterHolding":76,
-          "LockendDate":"2025-10-15"
+          "LockendDate":"2025-09-27"
        },
        {
           "SN":28,
@@ -250,7 +250,7 @@ export default
           "NumberOfShares":32000000,
           "PromoterShares":24000000,
           "PromoterHolding":75,
-          "LockendDate":"2025-12-10"
+          "LockendDate":"2025-11-23"
        },
        {
           "SN":29,
@@ -259,7 +259,7 @@ export default
           "NumberOfShares":2500000,
           "PromoterShares":1800000,
           "PromoterHolding":72,
-          "LockendDate":"2026-01-01"
+          "LockendDate":"2026-12-18"
        },
        {
           "SN":30,
@@ -268,7 +268,7 @@ export default
           "NumberOfShares":21000000,
           "PromoterShares":17850000,
           "PromoterHolding":85,
-          "LockendDate":"2026-02-19"
+          "LockendDate":"2026-02-06"
        },
        {
           "SN":31,
@@ -277,7 +277,7 @@ export default
           "NumberOfShares":10000000,
           "PromoterShares":6600000,
           "PromoterHolding":66,
-          "LockendDate":"2026-02-22"
+          "LockendDate":"2026-02-09"
        },
        {
           "SN":32,
@@ -286,7 +286,7 @@ export default
           "NumberOfShares":2000000,
           "PromoterShares":1400000,
           "PromoterHolding":70,
-          "LockendDate":"2026-02-23"
+          "LockendDate":"2026-02-10"
        },
        {
           "SN":33,
@@ -295,7 +295,7 @@ export default
           "NumberOfShares":5000000,
           "PromoterShares":4000000,
           "PromoterHolding":80,
-          "LockendDate":"2026-03-02"
+          "LockendDate":"2026-03-17"
        },
        {
           "SN":34,
@@ -304,7 +304,7 @@ export default
           "NumberOfShares":3400000,
           "PromoterShares":2219860,
           "PromoterHolding":65,
-          "LockendDate":"2026-03-11"
+          "LockendDate":"2026-02-26"
        },
        {
           "SN":35,
@@ -313,7 +313,7 @@ export default
           "NumberOfShares":8025000,
           "PromoterShares":4494000,
           "PromoterHolding":56,
-          "LockendDate":"2026-03-16"
+          "LockendDate":"2026-03-03"
        },
        {
           "SN":36,
@@ -322,7 +322,7 @@ export default
           "NumberOfShares":33325000,
           "PromoterShares":23327500,
           "PromoterHolding":70,
-          "LockendDate":"2026-04-04"
+          "LockendDate":"2026-03-22"
        },
        {
           "SN":37,
@@ -331,7 +331,7 @@ export default
           "NumberOfShares":11200000,
           "PromoterShares":5712000,
           "PromoterHolding":51,
-          "LockendDate":"2026-04-13"
+          "LockendDate":"2026-03-31"
        },
        {
           "SN":38,
@@ -340,7 +340,7 @@ export default
           "NumberOfShares":7600000,
           "PromoterShares":5320000,
           "PromoterHolding":70,
-          "LockendDate":"2026-04-24"
+          "LockendDate":"2026-04-12"
        },
        {
           "SN":39,
@@ -349,7 +349,7 @@ export default
           "NumberOfShares":5440534,
           "PromoterShares":3808373,
           "PromoterHolding":70,
-          "LockendDate":"2026-04-28"
+          "LockendDate":"2026-04-16"
        },
              {
           "SN":40,
@@ -358,7 +358,7 @@ export default
           "NumberOfShares":5364860,
           "PromoterShares":3755402,
           "PromoterHolding":70,
-          "LockendDate":"2026-05-05"
+          "LockendDate":"2026-04-21"
        },
        {
           "SN":41,
@@ -367,7 +367,7 @@ export default
           "NumberOfShares":5398083,
           "PromoterShares":3778658,
           "PromoterHolding":70,
-          "LockendDate":"2026-05-10"
+          "LockendDate":"2026-04-26"
        },
        {
           "SN":42,
@@ -376,7 +376,7 @@ export default
           "NumberOfShares":3921568,
           "PromoterShares":1999999,
           "PromoterHolding":51,
-          "LockendDate":"2026-05-16"
+          "LockendDate":"2026-05-02"
        },
        {
           "SN":43,
@@ -385,7 +385,7 @@ export default
           "NumberOfShares":6000000,
           "PromoterShares":4200000,
           "PromoterHolding":70,
-          "LockendDate":"2026-05-24"
+          "LockendDate":"2026-05-10"
        },
        {
           "SN":44,
@@ -394,7 +394,7 @@ export default
           "NumberOfShares":29000000,
           "PromoterShares":21750000,
           "PromoterHolding":75,
-          "LockendDate":"2026-05-28"
+          "LockendDate":"2026-05-14"
        },
        {
           "SN":45,
@@ -403,7 +403,7 @@ export default
           "NumberOfShares":2200000,
           "PromoterShares":1329900,
           "PromoterHolding":60,
-          "LockendDate":"2026-07-19"
+          "LockendDate":"2026-07-07"
        },
        {
           "SN":46,
@@ -412,7 +412,7 @@ export default
           "NumberOfShares":39719006,
           "PromoterShares":31775204,
           "PromoterHolding":80,
-          "LockendDate":"2026-07-28"
+          "LockendDate":"2026-07-16"
        }
     ]
  
