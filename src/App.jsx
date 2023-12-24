68ebac2197c43330403f4f789007644f5fdc388a
Nabin Kandel <nabinkandel@student.pu.edu.np>
2023-12-24 18:17:48 +0545

    GA-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Table from './Table'
+import ReactGA from 'react-ga'
 import Contributors from './Contributors'
+
+const TRACKING_ID = "UA-284093981-1"
+
+ReactGA.initialize(TRACKING_ID);
+
+
+
 function App() {
+
+  useEffect(() => {ReactGA.pageview(window.location.pathname)},[])
+
   return (
     <>
     <Table/>
